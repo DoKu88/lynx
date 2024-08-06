a470b16b7e1b0cfd26725a0da50cce8e64e85b3f
doku88 <ctmk2020@gmail.com>
2024-08-06 16:17:42 -0700

    added delimiter token between user & prompt

--- trainer/trainLLM.py
+++ trainer/trainLLM.py
@@ -1,13 +1,19 @@
 import json
 from datasets import Dataset
 from transformers import T5Tokenizer, T5ForConditionalGeneration, TrainingArguments, Trainer
+import torch
 
 # Load your custom dataset
 with open('data.json') as f:
     data = json.load(f)
 
 # Convert to Hugging Face Dataset
-dataset = Dataset.from_dict({"text": [item["text"] for item in data], "summary": [item["summary"] for item in data]})
+
+# Define the dataset
+dataset = Dataset.from_dict({
+                            "user": [item["user"] for item in data],
+                            "text": [item["text"] for item in data], 
+                            "summary": [item["summary"] for item in data]})
 
 # Split into train and validation sets
 split_datasets = dataset.train_test_split(test_size=0.1)
@@ -19,16 +25,32 @@ model_name = 't5-small'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 
 # Preprocess the dataset
+# Add a delimiter between the user and the text and tokenize the inputs
+# Delimiter added for ease of training to denote users and the text we want 
 def preprocess_function(examples):
-    inputs = [doc for doc in examples['text']]
-    model_inputs = tokenizer(inputs, max_length=512, truncation=True, padding="max_length")
+    users = [doc for doc in examples['user']]
+    text = [doc for doc in examples['text']]
+
+    userTokens = tokenizer(users, max_length=512, truncation=True, padding="max_length", return_tensors='pt')
+    textTokens = tokenizer(text, max_length=512, truncation=True, padding="max_length", return_tensors='pt')
+
+    delimiter_token = '[DELIMITER]'
+    tokenizer.add_tokens([delimiter_token])
+    delimiter_token_id = tokenizer.convert_tokens_to_ids(delimiter_token)
+
+    # Concatenate user and question encodings with the delimiter token in between
+    input_ids = [torch.cat([user, torch.tensor([delimiter_token_id]), text]) for user, text in zip(userTokens['input_ids'], textTokens['input_ids'])]
+    attention_mask = [torch.cat([user, torch.tensor([1]), text]) for user, text in zip(userTokens['attention_mask'], textTokens['attention_mask'])]
+
+    # The final encodings that can be used as input to the model
+    final_encodings = {'input_ids': torch.stack(input_ids), 'attention_mask': torch.stack(attention_mask)}
 
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         labels = tokenizer(examples['summary'], max_length=150, truncation=True, padding="max_length")
 
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
+    final_encodings["labels"] = labels["input_ids"]
+    return final_encodings
 
 # Tokenize the datasets
 tokenized_train_dataset = train_dataset.map(preprocess_function, batched=True)
