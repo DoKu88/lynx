7427b3d3b368ed7d44e43f64f869410c305df87f
doku88 <ctmk2020@gmail.com>
2024-08-07 18:04:01 -0700

    added ability to get YouTube transcript; no speaker identification though

--- scraper/youtubeScrapeTest.py
+++ scraper/youtubeScrapeTest.py
@@ -1,4 +1,5 @@
 import yt_dlp
+from youtube_transcript_api import YouTubeTranscriptApi
 
 def download_video(url, output_path='../data'):
 
@@ -10,9 +11,18 @@ def download_video(url, output_path='../data'):
         ydl.download([url])
 
 def download_video_from_metadata(video_metadata_lst, output_path='../data'):
-
     for video_metadata in video_metadata_lst:
         download_video(video_metadata['url'], output_path)
+
+def download_transcript_from_metadata(video_metadata_lst, output_path='../data'):
+
+    for video_metadata in video_metadata_lst:
+        video_id = video_metadata['id']
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+        with open(f'{output_path}/{video_id}.txt', 'w') as f:
+            for entry in transcript:
+                f.write(f"{entry['start']} - {entry['duration']}: {entry['text']}\n")
 
 def get_channel_videos(channel_url):
     ydl_opts = {
@@ -53,7 +63,7 @@ def lex_fridman_channel_videos(video_metadata, min_duration=600, max_duration=25
 
     return True
 
-def get_metadata_from_videos(channel_videos):
+def get_metadata_from_videos(channel_videos, channel_category):
     video_entries = channel_videos['entries'][0]['entries']
     print("Available Keys: ", video_entries[0].keys())
 
@@ -61,13 +71,17 @@ def get_metadata_from_videos(channel_videos):
 
     for video in video_entries:
 
-        if lex_fridman_channel_videos(video, restrict_guests=['mit']):
+        if channel_category == 'lex_fridman':
+            if not lex_fridman_channel_videos(video, restrict_guests=['mit', 'ai', 'and']):
+                continue
+                
             video_metadata.append(video)
 
             print('Title: ', video['title'])
             print('Duration: ', video['duration'])
             print('Names: ', video['names'])
             print('URL: ', video['url'])
+            print('id: ', video['id'])
 
     print('Total Videos: ', len(video_metadata))
 
@@ -75,9 +89,10 @@ def get_metadata_from_videos(channel_videos):
 
 if __name__ == '__main__':
     channel_videos = get_channel_videos('https://www.youtube.com/@lexfridman')
-    video_metadata_lst = get_metadata_from_videos(channel_videos)
+    video_metadata_lst = get_metadata_from_videos(channel_videos, 'lex_fridman')
 
     print(video_metadata_lst[0])
     import pdb; pdb.set_trace() # break point before we decide to actually download something
-    download_video_from_metadata(video_metadata_lst[:1], '../data/videoDownloads')
+    download_transcript_from_metadata(video_metadata_lst[:1], '../data/transcripts') # put [:1] to not use a ton of data
+    #download_video_from_metadata(video_metadata_lst[:1], '../data/videoDownloads') # put [:1] to not use a ton of data
 
